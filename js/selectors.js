bf385c2383610b71820d4bee53a523695184a577
LaRaye Johnson <lnj2105@gmail.com>
2019-09-08 21:03:48 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  const elements = $('p');
+  return elements;
+}
+
+function lastImageSelector() {
+  const image = $('div img:last');
+  return image;
+}
+
+function ninjaBabySelector() {
+  const ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector() {
+  const divs = $('.pics');
+  return divs;
+}
+
+function firstListItem() {
+  const firstItem = $('ul#pic-list li:first-child');
+  return firstItem;
+}
